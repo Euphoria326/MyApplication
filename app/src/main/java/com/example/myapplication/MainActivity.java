27b0f8d7e14b0d509bbe8d52cd1bf2f9857cfbfa
Euphoria326 <1184664874@qq.com>
2023-07-13 16:18:52 +0800

    player

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+import static android.os.Build.VERSION_CODES.R;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(com.example.myapplication.R.layout.activity_main);
+        Button button=(Button) findViewById(com.example.myapplication.R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent play=new Intent(MainActivity.this,player.class);
+                startActivity(play);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myapplication/player.java
+++ app/src/main/java/com/example/myapplication/player.java
@@ -6,48 +6,64 @@ import android.animation.ObjectAnimator;
 import android.media.MediaParser;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static android.os.Build.VERSION_CODES.M;
+import static android.os.Build.VERSION_CODES.R;
 
 public class player extends AppCompatActivity {
-    private int flag=1;
-    private int pause=1;
-    private  MediaPlayer media =null;
+    private int flag = 1;
+    private int pause = 1;
+    private MediaPlayer media = null;
+    private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.player);
-        ImageView im1=(ImageView) findViewById(R.id.imageView);
-        ImageView im2=(ImageView) findViewById(R.id.imageView2);
-        ImageView im3=(ImageView)findViewById(R.id.imageView3);
-        ObjectAnimator anim = ObjectAnimator.ofFloat(im3,"rotation",0,360);
-        anim.setDuration(3000);
+        setContentView(com.example.myapplication.R.layout.player);
+        ImageView im1 = (ImageView) findViewById(com.example.myapplication.R.id.imageView);
+        ImageView im2 = (ImageView) findViewById(com.example.myapplication.R.id.imageView2);
+        ImageView im3 = (ImageView) findViewById(com.example.myapplication.R.id.imageView3);
+        ObjectAnimator anim = ObjectAnimator.ofFloat(im3, "rotation", 0, 360);
+        anim.setDuration(8000);
         anim.setInterpolator(new LinearInterpolator());
-        anim.setRepeatCount(5);
+        anim.setRepeatCount(ObjectAnimator.INFINITE);
         anim.setRepeatMode(ObjectAnimator.RESTART);
-        media=MediaPlayer.create(this,R.raw.beautiful);
-        SeekBar seekBar=(SeekBar)findViewById(R.id.seekBar);
+        media = MediaPlayer.create(this, com.example.myapplication.R.raw.beautiful);
+        SeekBar seekBar = (SeekBar) findViewById(com.example.myapplication.R.id.seekBar);
+        seekBar.setMax(media.getDuration());
+        TextView text1 = (TextView) findViewById(com.example.myapplication.R.id.textView);
+        TextView text2 = (TextView) findViewById(com.example.myapplication.R.id.textView2);
+        String duration = musictime(media.getDuration());
+        text1.setText(duration);
+
         im1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 im1.setVisibility(View.INVISIBLE);
                 im2.setVisibility(View.VISIBLE);
                 LinearInterpolator lin = new LinearInterpolator();//设置动画匀速运动
-                if(flag==1) {
+                if (flag == 1) {
                     anim.start();
-                    flag=0;
-                }
-                else if (flag==0) anim.resume();
-                if(pause==1){
+                    flag = 0;
+                } else if (flag == 0) anim.resume();
+                if (pause == 1) {
                     media.seekTo(media.getCurrentPosition());
                     media.start();
-                    pause=0;
-                }else{
+                    pause = 0;
+                } else {
                     media.start();
                 }
             }
@@ -58,26 +74,54 @@ public class player extends AppCompatActivity {
                 im2.setVisibility(View.INVISIBLE);
                 im1.setVisibility(View.VISIBLE);
                 anim.pause();
-                if(media.isPlaying()){
+                if (media.isPlaying()) {
                     media.pause();
-                    pause=1;
+                    pause = 1;
                 }
             }
         });
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                seekBar.setProgress(media.getCurrentPosition());
+            }
+        }, 0, 1000);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-
+                if (b) {
+                    media.seekTo(i);
+                    seekBar.setProgress(i);
+                }
+                String current= musictime(media.getCurrentPosition());
+                text2.setText(current);
             }
+
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
 
             }
+
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 media.seekTo(seekBar.getProgress());
-                //松开之后音乐跳转到相应位置
+
             }
         });
     }
+    public String musictime(int t) {
+        String time;
+        int min=t/1000/60;
+        int sec=t/1000%60;
+        time="0"+min+":";
+        if(sec<10) time+="0";
+        time+=sec;
+        return time;
+    }
+
+
+
 }
+
+
