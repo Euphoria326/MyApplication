aa6467924db6ac74c8603e93da5295a101fb93b8
C.Leclerc <lx530111@dlmu.edu.cn>
2023-07-15 22:54:30 +0800

    1145114

--- app/src/main/java/com/example/myapplication/List.java
+++ app/src/main/java/com/example/myapplication/List.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class List extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class List extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
     }
+
+
 }
