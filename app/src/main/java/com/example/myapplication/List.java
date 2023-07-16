54b533a8aad57e7cb1788f58ee0dd5bdb2023997
C.Leclerc <lx530111@dlmu.edu.cn>
2023-07-16 23:22:21 +0800

    1145114

--- app/src/main/java/com/example/myapplication/List.java
+++ app/src/main/java/com/example/myapplication/List.java
@@ -3,14 +3,20 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 
 public class List extends AppCompatActivity {
 
+    private ImageButton ib1,ib2,ib3,ib4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        ib1=findViewById(R.id.playthelist);
+        ib2=findViewById(R.id.random_playlist);
+        ib3=findViewById(R.id.exit);
+
 
     }
 
