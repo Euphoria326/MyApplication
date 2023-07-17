9703173b707fa5fd46ab85791886af22a12015b1
C.Leclerc <lx530111@dlmu.edu.cn>
2023-07-18 00:36:37 +0800

    1145114

--- app/src/main/java/com/example/myapplication/List.java
+++ app/src/main/java/com/example/myapplication/List.java
@@ -2,13 +2,16 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 
 public class List extends AppCompatActivity {
 
     private ImageButton ib1,ib2,ib3,ib4;
+    private List list1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +20,13 @@ public class List extends AppCompatActivity {
         ib2=findViewById(R.id.random_playlist);
         ib3=findViewById(R.id.exit);
 
+        ib3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent exitmain= new Intent(List.this,MainActivity.class);
+                startActivity(exitmain);
+            }
+        });
 
     }
 
